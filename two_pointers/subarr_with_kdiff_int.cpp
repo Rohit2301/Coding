b4e7ed72108c88183117f362a5d173d2014791ad
Rohit Duhan <rohitduhan2301@gmail.com>
2022-02-02 10:24:45 +0530

    subarray with K diff no

--- two_pointers/subarr_with_kdiff_int.cpp
+++ two_pointers/subarr_with_kdiff_int.cpp
@@ -1,6 +1,18 @@
 #include <bits\stdc++.h>
 using namespace std;
 
+void remove(int c, unordered_map<int, int> mapr)
+{
+    if (mapr[c] == 1)
+    {
+        mapr.erase(c);
+    }
+    else
+    {
+        mapr[c]--;
+    }
+}
+
 int main()
 {
     vector<int> nums(5);
@@ -15,19 +27,111 @@ int main()
     unordered_map<int, int> big;
     unordered_map<int, int> small;
 
-    int str = 0;
     int cnt = 0;
+    int is = -1;
+    int ib = -1;
+    int j = -1;
 
-    
+    if (k == 1)
+    {
+        while (true)
+        {
+            int f1 = 1;
+            int f2 = 1;
+            while (ib < nums.size() - 1)
+            {
+                f1 = 0;
+                ib++;
+                big[nums[ib]]++;
+                if (big.size() == 2)
+                {
+                    remove(nums[ib], big);
+                    ib--;
+                    break;
+                }
+            }
+            while (j < ib)
+            {
+                f2 = 0;
+                if (big.size() == 1)
+                {
+                    cnt += ib - j;
+                    cout << cnt << " ";
+                }
+                j++;
+                remove(nums[j], big);
+                if (big.size() < 1)
+                {
+                    break;
+                }
+            }
+            if (f1 == 1 && f2 == 1)
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
 
-    
+        while (true)
+        {
+
+            int f1 = 1;
+            int f2 = 1;
+            int f3 = 1;
+
+            while (ib < (nums.size() - 1))
+            {
+                cout << ib << " " << nums.size();
+                ib++;
+                cout << ib;
+                f1 = 0;
+                big[nums[ib]]++;
+                if (big.size() == k + 1)
+                {
+                    remove(nums[ib], big);
+                    ib--;
+                    break;
+                }
+            }
+            while (is < ib)
+            {
+                f2 = 0;
+                is++;
+                small[nums[is]]++;
+                if (small.size() == k)
+                {
+                    remove(nums[is], small);
+                    is--;
+                    break;
+                }
+            }
+            while (j < is)
+            {
+                f3 = 0;
+                if (big.size() == k && small.size() == k - 1)
+                {
+                    cnt += ib - is;
+                    cout << cnt << " ";
+                }
+                j++;
+                remove(nums[j], small);
+                remove(nums[j], big);
+                if (big.size() < k || small.size() < k - 1)
+                {
+                    break;
+                }
+            }
+            if (f1 == 1 && f2 == 1 && f3 == 1)
+            {
+                break;
+            }
+            cout << 1;
+        }
+    }
+
     cout << "\ncount is " << cnt;
-
-    // for (auto x : freq)
-
-    // {
-    //     cout << x.second;
-    // }
 
     return 0;
 }
