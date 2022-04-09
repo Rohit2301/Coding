6d5e7d61bfb19b6b83840f06be68d7e9ba9b43ea
Rohit Duhan <rohitduhan2301@gmail.com>
2022-04-09 12:09:50 +0530

    subsetsss

---
+++ nados_boiler_plate_code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permutations(vector<int> boxes, int ci, int ti)
+{
+    // write your code here
+
+    cout << ci << " here" << ti;
+}
+
+int main()
+{
+    int nbox;
+    cin >> nbox;
+    int ritem;
+    cin >> ritem;
+    vector<int> num(nbox);
+    permutations(num, nbox, ritem);
+
+    return 0;
+}
--- recursion/subsets.cpp
+++ recursion/subsets.cpp
@@ -1,37 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> comb(int n, int i)
+// vector<vector<int>> comb(int n, int i)
+// {
+//     if (i > n)
+//     {
+//         vector<vector<int>> fnd = {{}};
+//         return fnd;
+//     }
+//     vector<vector<int>> fndY;
+//     vector<vector<int>> fndN;
+//     vector<vector<int>> ans;
+
+//     fndY = comb(n, i + 1);
+//     for (int j = 0; j < fndY.size(); j++)
+//     {
+//         fndY[j].push_back(i);
+//         ans.push_back(fndY[j]);
+//     }
+//     fndN = comb(n, i + 1);
+//     for (int j = 0; j < fndN.size(); j++)
+//     {
+//         ans.push_back(fndN[j]);
+//     }
+
+//     return ans;
+// }
+
+vector<vector<int>> res;
+
+void comb(vector<int> curr, int n, int k, int str)
 {
-    if (i > n)
+    res.push_back(curr);
+    for (int i = str; i < n + 1; i++)
     {
-        vector<vector<int>> fnd = {{}};
-        return fnd;
+        curr.push_back(i);
+        comb(curr, n, k, i + 1);
+        curr.pop_back();
     }
-    vector<vector<int>> fndY;
-    vector<vector<int>> fndN;
-    vector<vector<int>> ans;
-
-    fndY = comb(n, i + 1);
-    for (int j = 0; j < fndY.size(); j++)
-    {
-        fndY[j].push_back(i);
-        ans.push_back(fndY[j]);
-    }
-    fndN = comb(n, i + 1);
-    for (int j = 0; j < fndN.size(); j++)
-    {
-        ans.push_back(fndN[j]);
-    }
-
-    return ans;
 }
-
+void combine(int n, int k)
+{
+    vector<int> curr;
+    comb(curr, n, k, 1);
+    return;
+}
 int main()
 {
     vector<vector<int>> ans;
-    ans = comb(3, 1);
-    for (vector<int> i : ans)
+    combine(3, 1);
+    for (vector<int> i : res)
     {
         for (int num : i)
         {
